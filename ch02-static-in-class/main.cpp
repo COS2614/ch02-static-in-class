6131aed3f6c4da4d4a2d3f72fe49bed6884eb896
mitchcamza <192mitch88@gmail.com>
2023-03-28 13:10:20 +0200

    Define class with static data members and static member functions to demonstrate thier use in a class

--- ch02-static-in-class/main.cpp
+++ ch02-static-in-class/main.cpp
@@ -1,8 +1,28 @@
-#include <QCoreApplication>
+#include <QtCore>
+
+
+class MyClass
+{
+private:
+    static int m_InstanceCount;
+
+public:
+    MyClass() { m_InstanceCount++; }
+    ~MyClass() { m_InstanceCount--; }
+
+    static int getInstanceCount() { return m_InstanceCount; }
+};
+
+
+int MyClass::m_InstanceCount = 0;
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    return a.exec();
+    MyClass obj1, obj2, obj3;
+    qDebug() << "Number of instances created: " << MyClass::getInstanceCount();
+
+    return 0;
 }
