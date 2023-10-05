29553f7a38773ed416b0a9a8a6ce2dfd7e9c8e17
mitchcamza <192mitch88@gmail.com>
2023-10-05 09:39:49 +0200

    Fix folder structure naming

--- ch02-static-in-class/main.cpp
+++ ch02-static-in-class/main.cpp
@@ -17,12 +17,19 @@ public:
 int MyClass::m_InstanceCount = 0;
 
 
+void createInstancesOnStack()
+{
+    MyClass obj1, obj2, obj3;
+    qDebug() << "Number of instances created on the stack: " << MyClass::getInstanceCount();
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    MyClass obj1, obj2, obj3;
-    qDebug() << "Number of instances created: " << MyClass::getInstanceCount();
+    createInstancesOnStack();   // Destructor is called when stack objects go out of scope
+    qDebug() << "Objects created on the stack have been deleted";
+    qDebug() << "Number of instances remaining: " << MyClass::getInstanceCount();
 
     return 0;
 }
